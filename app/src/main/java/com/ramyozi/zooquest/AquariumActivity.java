48fc83b18d77a5f5a451a51d53c4e4ca4027b0d6
Ramzi BENMANSOUR <ramzibenmansour27@gmail.com>
2023-05-17 09:46:58 +0200

    onTouchEvent() added to the CardActivity class

--- app/src/main/java/com/ramyozi/zooquest/AquariumActivity.java
+++ app/src/main/java/com/ramyozi/zooquest/AquariumActivity.java
@@ -1,48 +1,43 @@
 package com.ramyozi.zooquest;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.content.Context;
-import android.graphics.Canvas;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
-public class CardActivity extends Activity {
+public class AquariumActivity extends Activity {
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new CardView(this));
-        Log.i("CardActivity","onCreate() is done");
+        setContentView(new AquariumView(this));
+        Log.i("AquariumActivity","onCreate() is done");
         Toast.makeText(this,"Welcome", Toast.LENGTH_LONG).show();
+
     }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        if (event.getActionMasked() == MotionEvent.ACTION_DOWN){
-            Intent intent = new Intent(this, AquariumActivity.class);
-            startActivity(intent);
-        }
-        return super.onTouchEvent(event);
-    }
 
-    class CardView extends View {
-        public CardView(Context context) {
+
+    class AquariumView extends View {
+        public AquariumView(Context context) {
             super(context);
         }
 
         @Override
         protected void onDraw(Canvas canvas) {
             //super.onDraw(canvas);
-            Bitmap b = BitmapFactory.decodeResource(getResources(),R.drawable.ippo);
+            Bitmap b = BitmapFactory.decodeResource(getResources(),R.drawable.takamura);
             canvas.drawBitmap(b,0,0,null);
-            Log.i("CardActivity","Card : " + b.getWidth()+"/"+b.getHeight());
+            Log.i("AquariumActivity","Aquarium : " + b.getWidth()+"/"+b.getHeight());
         }
     }
 }
