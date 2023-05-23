0f77f037f71b03dddf9656e05394182d439f769f
Ramzi BENMANSOUR <ramzibenmansour27@gmail.com>
2023-05-23 17:13:23 +0200

    AutoCompleteTextView and ArrayAdapter used to allow user to choose from already specified selection in the AlerteActivity file

--- app/src/main/java/fr/ldnr/ramyozi/ZooQuest/AlerteActivity.java
+++ app/src/main/java/fr/ldnr/ramyozi/ZooQuest/AlerteActivity.java
@@ -1,10 +1,16 @@
 package fr.ldnr.ramyozi.ZooQuest;
 
+
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -14,10 +20,30 @@ public class AlerteActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.alerte);
+        AutoCompleteTextView etLieu = findViewById(R.id.et_lieu);
+        String[] lieux = getResources().getStringArray(R.array.alerte_lieux);
+        ArrayAdapter<String> aa = new ArrayAdapter<>(this,
+                android.R.layout.simple_dropdown_item_1line, lieux);
+        etLieu.setAdapter(aa);
     }
 
-    public void envoyer(View view) {
-        //TODO Afficher un ! au bout si l'alerte est urgente
+        public void envoyer(View view) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(R.string.alerte_titre);
+            builder.setMessage(R.string.alerte_confirmer);
+            builder.setIcon(R.mipmap.ic_launcher);
+            builder.setPositiveButton(android.R.string.yes,
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            confirmer();
+                        }
+                    });
+            builder.setNegativeButton(android.R.string.no, null);
+            builder.show();
+        }
+
+    public void confirmer() {
         EditText etIntitule = findViewById(R.id.et_intitule);
         String intitule = etIntitule.getText().toString();
         CheckBox cbUrgent = findViewById(R.id.cb_urgent);
